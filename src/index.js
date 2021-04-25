705b9d0af10afa8b4dfe1f1169a6806e82005430
Mariia Kontseva <marishka95@ukr.net>
2021-04-25 15:52:30 +0300

    Work at server

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import menuItemsTpl from './templates/menu-items-info.hbs';
 import menuItemsInfo from './menu.json';
 import './css/styles.css';
 
+// Шаблонизация
 const menuContainer = document.querySelector('.js-menu');
 const menuMarkup = createMenuItemsMarkup(menuItemsInfo);
 
@@ -9,4 +10,38 @@ menuContainer.insertAdjacentHTML('beforeend', menuMarkup);
 
 function createMenuItemsMarkup(menuItemsInfo) {
     return menuItemsTpl(menuItemsInfo);
+}
+
+// Тема
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+const themeCheckbox = document.querySelector('#theme-switch-toggle');
+themeCheckbox.addEventListener('change', onCheckboxStatusChange);
+const body = document.querySelector('body');
+
+body.classList = localStorage.getItem('theme');
+if (localStorage.getItem('theme') === 'dark-theme'){ 
+    themeCheckbox.checked = true; 
+}
+
+function onCheckboxStatusChange() {
+    if (themeCheckbox.checked === true) {
+        
+        if (body.classList.contains(Theme.LIGHT)){
+            body.classList.remove(Theme.LIGHT);
+        }
+        body.classList.add(Theme.DARK);
+        themeCheckbox.setAttribute('checked', true);
+        localStorage.setItem('theme', body.classList);
+    }
+        
+    else {
+        if (body.classList.contains(Theme.DARK)){
+            body.classList.remove(Theme.DARK);
+        }
+        body.classList.add(Theme.LIGHT);
+        localStorage.setItem('theme', body.classList);
+    } 
 }
